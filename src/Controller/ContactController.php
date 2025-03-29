ee18714c4c6880df59611ba3cd91f632bcb37f20
Alpha75Next-Evolution <alphady.mondzali@next-evolution.it.com>
2025-03-29 08:03:57 +0100

    Intégration template

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/contact')]
+final class ContactController extends AbstractController
+{
+    #[Route('/', name: 'contact')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
